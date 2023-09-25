0561b31bb89495003fadf81a30904704e4e9a002
小李同学 <1620123160@qq.com>
2023-09-25 21:59:39 +0800

    配置scss-loading

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './less/public.less'
+import './scss/public.scss'
 ReactDOM.render(
     <App />,
     document.getElementById("app")
--- webpack.config.js
+++ webpack.config.js
@@ -33,11 +33,22 @@ module.exports = {
             {
                 test: /\.less$/,
                 use: [{
+                    loader: 'style-loader' // creates style nodes from JS strings
+                }, {
+                    loader: 'css-loader' // translates CSS into CommonJS
+                }, {
+                    loader: 'less-loader' // compiles Less to CSS
+                }]
+            },
+            //scss
+            {
+                test: /\.scss$/,
+                use: [{
                 loader: 'style-loader' // creates style nodes from JS strings
                 }, {
                 loader: 'css-loader' // translates CSS into CommonJS
                 }, {
-                loader: 'less-loader' // compiles Less to CSS
+                loader: 'sass-loader' // compiles sass to CSS
                 }]
                 }
                 
