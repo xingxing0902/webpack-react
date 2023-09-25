e93917f6444644b7022d9896109bb97c9534ec4b
小李同学 <1620123160@qq.com>
2023-09-25 21:52:48 +0800

    配置less-loading

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './css/public.css'
+import './less/public.less'
 ReactDOM.render(
     <App />,
     document.getElementById("app")
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,18 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
+            //less
+            {
+                test: /\.less$/,
+                use: [{
+                loader: 'style-loader' // creates style nodes from JS strings
+                }, {
+                loader: 'css-loader' // translates CSS into CommonJS
+                }, {
+                loader: 'less-loader' // compiles Less to CSS
+                }]
+                }
+                
 
         ]
     },
