4924dad1581ec6e689c873453b03be2ed0bd7e3c
小李同学 <1620123160@qq.com>
2023-09-25 19:35:14 +0800

    配置多入口文件

---
+++ src/one.js
@@ -0,0 +1,4 @@
+import Two from './two'
+const x=100;
+console.log(x);
+alert(Two.y);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
 module.exports={
-    entry:'./src/index.js',
+    entry:{
+        index:"./src/index.js",
+        one:"./src/one.js"
+        },
+        
     output:{
         path:path.resolve(__dirname,"dist"),
-        filename:'main.js'
+        filename:"[name].main.js"
     },
     mode:'production'
 }
