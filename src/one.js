ffb17c3cd949c45427b03e3934bbca6ccf372eda
小李同学 <1620123160@qq.com>
2023-09-25 20:19:22 +0800

    装饰器

--- src/one.js
+++ src/one.js
@@ -1,5 +1,13 @@
 import Two from './two.js'
-const test=(num)=>{
-console.log("test函数哈哈"+num);
+const test = (num) => {
+    console.log("test函数哈哈" + num);
 }
 test(Two.y);
+function testable(target) {
+    target.isTestable = true;
+    }
+    @testable
+    class MyTestableClass {}
+    console.log("装饰器语法",MyTestableClass.isTestable) // true
+    
+
